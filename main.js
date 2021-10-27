8e5d05f58015780414bf5de93417dd420ef9c958
lorenapassagem <lorenapassagem@gmail.com>
2021-10-27 20:46:01 -0300

    :tada: create first components cardgame
    git remote add origin https://github.com/LorenaPassagem/curso-js-para-react.git

---
+++ main.js
@@ -0,0 +1,9 @@
+import CardGame from "./src/components/CardGame";
+
+import './src/styles/settings/colors.css';
+import './src/styles/elements/base.css';
+import './src/styles/generic/reset.css'
+
+const $root = document.querySelector("#root");
+const $htmlCardGame = CardGame()
+$root.insertAdjacentHTML("afterbegin", $htmlCardGame)
